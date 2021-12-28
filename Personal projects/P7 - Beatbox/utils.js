3d62c62c619a02a655b2e9aa84dce40600ab9655
Alvi <djalvee26@gmail.com>
2021-12-28 14:47:32 +0600

    5th task done

--- Personal projects/P7 - Beatbox/utils.js
+++ Personal projects/P7 - Beatbox/utils.js
@@ -17,20 +17,40 @@ class Beat {
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-  constructor(color, keyCode) {}
+  constructor(color, keyCode) {
+    this.color = color;
+    this.keyCode = keyCode;
+    this.element = document.getElementById(this.keyCode);
+    this.setButtonColorInHTML();
+    this.setTransitionEndListener();
+  }
 
   /**
    * Set the button color based on color specified
    */
-  setButtonColorInHTML = () => {};
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color;
+  };
+
+  setTransitionEndListener = () => {
+    this.element.addEventListener("transitionend", () => {
+      this.deselect();
+    });
+  };
 
   /**
    * Select function to set the background color and boxShadow
    */
-  select = () => {};
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+  };
 
   /**
    * Deselect function to reset background color and boxShadow
    */
-  deselect = () => {};
+  deselect = () => {
+    this.element.style.backgroundColor = "transparent";
+    this.element.style.boxShadow = `none`;
+  };
 }
