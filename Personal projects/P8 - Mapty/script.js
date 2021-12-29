1c41dfbd51387aafa77a7e12d554b8603a491a82
Alvi <djalvee26@gmail.com>
2021-12-29 12:55:34 +0600

    Displayed a map!

--- Personal projects/P8 - Mapty/script.js
+++ Personal projects/P8 - Mapty/script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      let { latitude } = position.coords;
+      let { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert("Couldn't find your location. Please try again!");
+    }
+  );
