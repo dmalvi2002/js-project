e1619f0604e02f8125b8532f409b87e54dacb6f6
Alvi <djalvee26@gmail.com>
2022-01-19 09:23:19 +0600

    Created my first js library

---
+++ JS - Library/pagination.js
@@ -0,0 +1,117 @@
+class PaginationMethod {
+  constructor(
+    container,
+    arr,
+    rowsPerPage,
+    currentPage,
+    btnContainer,
+    btnClass,
+    btnNext,
+    btnPrev,
+    pageNo
+  ) {
+    this.container = document.querySelector(container);
+    this.arr = arr;
+    this.rowsPerPage = rowsPerPage;
+    this.currentPage = currentPage;
+    this.btnContainer = document.querySelector(btnContainer);
+    this.btnClass = btnClass;
+    this.btnNext = document.querySelector(btnNext);
+    this.btnPrev = document.querySelector(btnPrev);
+    this.pageNo = document.querySelector(pageNo);
+
+    this.btnNext.addEventListener("click", this.nextPage);
+    this.btnPrev.addEventListener("click", this.prevPage);
+
+    this.changePage(this.currentPage);
+    this.createBtn(this.numPages());
+  }
+  numPages = () => {
+    return Math.ceil(this.arr.length / this.rowsPerPage);
+  };
+  prevPage = () => {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.changePage(this.currentPage);
+    }
+  };
+
+  nextPage = () => {
+    if (this.currentPage < this.numPages()) {
+      this.currentPage++;
+      this.changePage(this.currentPage);
+    }
+  };
+  changePage(page) {
+    let num = this.numPages();
+    if (page == 1) {
+      this.btnPrev.style.visibility = "hidden";
+    } else {
+      this.btnPrev.style.visibility = "visible";
+    }
+    if (page == num) {
+      this.btnNext.style.visibility = "hidden";
+    } else {
+      this.btnNext.style.visibility = "visible";
+    }
+
+    this.container.innerHTML = "";
+
+    for (
+      let i = (page - 1) * this.rowsPerPage;
+      i < page * this.rowsPerPage && i < this.arr.length;
+      i++
+    ) {
+      // Here you have to spicify about your element.
+      this.container.innerHTML += this.arr[i].adName + "<br>";
+    }
+    this.pageNo.innerHTML = page;
+  }
+  createBtn(num) {
+    for (let i = 1; i <= num; i++) {
+      let btn = document.createElement("button");
+      btn.innerText = i;
+      btn.classList.add(this.btnClass);
+      btn.addEventListener("click", () => {
+        this.changePage(btn.innerText);
+        this.currentPage = btn.innerText;
+      });
+      this.btnContainer.insertAdjacentElement("beforeend", btn);
+    }
+  }
+}
+let objJson = [
+  { adName: "AdName 1" },
+  { adName: "AdName 2" },
+  { adName: "AdName 3" },
+  { adName: "AdName 4" },
+  { adName: "AdName 5" },
+  { adName: "AdName 6" },
+  { adName: "AdName 7" },
+  { adName: "AdName 8" },
+  { adName: "AdName 9" },
+  { adName: "AdName 10" },
+  { adName: "AdName 11" },
+  { adName: "AdName 12" },
+  { adName: "AdName 13" },
+  { adName: "AdName 14" },
+  { adName: "AdName 15" },
+  { adName: "AdName 16" },
+  { adName: "AdName 17" },
+  { adName: "AdName 18" },
+  { adName: "AdName 19" },
+  { adName: "AdName 20" },
+  { adName: "AdName 21" },
+]; // Can be obtained from another source, such as your objJson variable
+
+const newPagination = new PaginationMethod(
+  ".listingTable",
+  objJson,
+  5,
+  1,
+  ".btn-container",
+  ".pagination-btn",
+  ".btn_next",
+  ".btn_prev",
+  ".page"
+);
--- Personal projects/P15 - Premium player/script.js
+++ Personal projects/P15 - Premium player/script.js
@@ -1,1 +1,103 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
 
+// Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
+
+// Check if playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Pause");
+  music.pause();
+}
+
+// Play Event
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Current song
+let songIndex = 0;
+
+// Next song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Update DOM
+function loadSong(song) {
+  artist.textContent = song.artist;
+  title.textContent = song.displayName;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Update progress bar & time
+function updateProgressBar() {
+  if (isPlaying) {
+    progress.style.width = `${(music.currentTime / music.duration) * 100}%`;
+  }
+}
+
+// Loads first song
+loadSong(songs[songIndex]);
+
+// Event listeners
+nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener("click", prevSong);
+music.addEventListener("timeupdate", updateProgressBar);
