49381fb17846e1d828ae23f98ac641015f2e8efa
Abu Bakar Siddique <80147468+dmalvi2002@users.noreply.github.com>
2022-06-17 03:10:52 +0600

    Delete Personal projects/P19 - Monsters rolodex directory

--- Personal projects/P19 - Monsters rolodex/src/Components/Search-box/search-box.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './search-box.styles.css';
-
-export const SearchInput = ({ placeholder, handleChange }) => {
-  return (
-    <input className='search' type="search" placeholder={placeholder} onInput={handleChange} />
-  )
-}
